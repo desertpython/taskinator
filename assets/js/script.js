73c8df6f2dc82f69ba2a206f19cd0b160d68b681
Desert Python <mattsenneff@aol.com>
2020-06-16 17:55:04 -0700

    disregard defaults

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 var createTaskHandler = function() {
     var listItemEl = document.createElement("li");
@@ -6,7 +6,8 @@ var createTaskHandler = function() {
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
 }
-buttonEl.addEventListener("click", createTaskHandler); {
+formEl.addEventListener("click", createTaskHandler); {
+  event.preventDefault();
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
   listItemEl.textContent = "This is a new task.";
