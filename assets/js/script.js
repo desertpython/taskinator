240db4104cc611befe92f304c37a8c9b8428c45b
Desert Python <mattsenneff@aol.com>
2020-06-16 21:25:16 -0700

    refactor

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
-var formEl = document.querySelector("#task-form");
+var formEl = document.querySelector("#save-task");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
-var taskFormHandler = function() {
+var taskFormHandler = function(event) {
   event.preventDefault();
   var taskNameInput = document.querySelector("input[name='task-name']").value;
   var taskTypeInput = document.querySelector("select[name='task-type']").value;
@@ -15,6 +15,7 @@ var taskFormHandler = function() {
   createTaskEl(taskDataObj);
 }
 
+
 var createTaskEl = function(taskDataObj) {
   // create list item
   var listItemEl = document.createElement("li");
@@ -24,7 +25,6 @@ var createTaskEl = function(taskDataObj) {
   var taskInfoEl = document.createElement("div");
   taskInfoEl.className = "task-info";
   taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
-
   listItemEl.appendChild(taskInfoEl);
 
   // add entire list item to list
